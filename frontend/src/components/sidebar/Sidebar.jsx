a943d9241413504165f19bc98be654ecc55dedcd
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2023-07-04 22:11:34 +0100

    Added template for SideBar

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,15 @@
 import './sidebar.scss'
-
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Person2Icon from '@mui/icons-material/Person2';
+import Inventory2Icon from '@mui/icons-material/Inventory2';
+import ReceiptIcon from '@mui/icons-material/Receipt';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import StackedBarChartIcon from '@mui/icons-material/StackedBarChart';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import MedicalInformationIcon from '@mui/icons-material/MedicalInformation';
+import HistoryIcon from '@mui/icons-material/History';
+import SettingsIcon from '@mui/icons-material/Settings';
+import LogoutIcon from '@mui/icons-material/Logout';
 const Sidebar = () => {
   return (
     <div className='sidebar'>
@@ -9,21 +19,65 @@ const Sidebar = () => {
         <hr/>
         <div className="center">
             <ul>
+                <p className="title">MAIN</p>
                 <li>
+                    <DashboardIcon className="icon"/>
                     <span>Dashboard</span>
                 </li>
+                <p className="title">LISTS</p>
                 <li>
-                    <span>Dashboard</span>
+                    <Person2Icon className="icon"/>
+                    <span>Users</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <Inventory2Icon className="icon"/>
+                    <span>Products</span>
                 </li>
                 <li>
-                    <span>Dashboard</span>
+                    <ReceiptIcon className="icon"/>
+                    <span>Orders</span>
+                </li>
+                <li>
+                    <LocalShippingIcon className="icon"/>
+                    <span>Delivery</span>
+                </li>
+                <p className="title">USEFUL</p>
+                <li>
+                    <StackedBarChartIcon className="icon"/>
+                    <span>Stats</span>
+                </li>
+                <li>
+                    <NotificationsActiveIcon className="icon"/>
+                    <span>Notifications</span>
+                </li>
+                <p className="title">SERVICE</p>
+                <li>
+                    <MedicalInformationIcon className="icon"/>
+                    <span>System Health</span>
+                </li>
+                <li>
+                    <HistoryIcon className="icon"/>
+                    <span>Logs</span>
+                </li>
+                <li>
+                    <SettingsIcon className="icon"/>
+                    <span>Settings</span>
+                </li>
+                <p className="title">USER</p>
+                <li>
+                    <ReceiptIcon className="icon"/>
+                    <span>Profile</span>
+                </li>
+                <li>
+                    <LogoutIcon className="icon"/>
+                    <span>Logout</span>
                 </li>
             </ul>
         </div>
-        <div className="bottom">color options</div>
+        <div className="bottom">
+            <div className="colorOption"></div>
+            <div className="colorOption"></div>
+        </div>
     </div>
   )
 }
