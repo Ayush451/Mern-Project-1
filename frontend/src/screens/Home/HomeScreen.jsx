1cc23de1060753ac8c0c003a0168e8775a304f2d
Ayush451 <88429944+Ayush451@users.noreply.github.com>
2023-07-05 01:28:08 +0100

    Added resuable widget components

--- frontend/src/screens/Home/HomeScreen.jsx
+++ frontend/src/screens/Home/HomeScreen.jsx
@@ -1,13 +1,23 @@
 import React from 'react'
 import Hero from '../../components/Hero'
 import Sidebar from '../../components/sidebar/Sidebar'
-
+import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widgets/Widget'
 const HomeScreen = () => {
   return (
 
     <div className='HomeScreen'>
         <Sidebar/>
-        <div className='homeContainer'>container</div>
+        <div className='homeContainer'>
+          <Navbar/>
+          <div className="widgets">
+            <Widget type="user"/>
+            <Widget type="order"/>
+            <Widget type="earning"/>
+            <Widget type="balance"/>
+          </div>
+          
+        </div>
     <Hero/>
     </div>
   )
